bbe628aa8ba2c15102bea6c8e00d7efdc57f20b9
sumomomomomo <matthew.hoph@outlook.sg>
2026-02-07 06:49:00 +0000

    Add placeholder image

--- app/index.js
+++ app/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World from Node.js!'));
+app.get('/', (req, res) => {
+    const htmlString = `
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Under Construction</h1>
+
+<img src="https://cdn.sumomo.horse/misc/unsu-true.jpg" alt="Seiun Sky" width="256" height="256">
+
+</body>
+</html>`;
+    res.send(htmlString)
+
+});
 
 app.listen(3000, () => console.log('App running on port 3000'));
